7ce3af9365da1122442d98f204feb7870ac331db
ananthshetty <107457713+ananthshetty@users.noreply.github.com>
2022-12-26 23:12:32 +0530

    Revert "donutChart_component"
    This reverts commit 76429c908455130649c7bf71631cb2efe4806056.

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # echartReactJs
 Echart by using npm package
-<!-- ApexChart>>Donut -->
-
-<!-- https://apexcharts.com/docs/installation/ -->
-<!-- npm install apexcharts --save -->
-
-<!-- apexChart - toolTip -->
-<!-- https://apexcharts.com/docs/options/tooltip/ -->
 
 <!-- command to install tailwind css -->
 npm install -D tailwindcss postcss autoprefixer
--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import styles from './App.module.scss';
-import DoughnutChart from './components/DoughnutChart/DoughnutChart';
 import KeenSliderComp from './components/KeenSliderComp/KeenSliderComp';
-import OverlappedCardsAnimation from './components/overlappedCardsAnimation/OverlappedCardsAnimation';
 import SimpleImageSlider from './components/SimpleImageSlider/SimpleImageSlider';
 
 
 function App() {
   return (
     <div className={` w-full h-[100vh] flex justify-center items-center ${styles.appContainer}`}>
-      {/* <KeenSliderComp/> */}
-      {/* <OverlappedCardsAnimation/> */}
-      <DoughnutChart />
+      <KeenSliderComp
+      />
     </div>
   );
 }
